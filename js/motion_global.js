c0551896f3bb94ba0f99087f589feccd513d0b72
selena <weizhezhe10@163.com>
2016-12-13 17:34:54 +0800

    Site updated: 2016-12-13 17:34:54

--- js/motion_global.js
+++ js/motion_global.js
@@ -1,7 +1,20 @@
-/* global NexT: true */
-
 $(document).ready(function () {
-  NexT.motion = {};
+  var motionIntegrator = {
+    queue: [],
+    cursor: -1,
+    add: function (fn) {
+      this.queue.push(fn);
+      return this;
+    },
+    next: function () {
+      this.cursor++;
+      var fn = this.queue[this.cursor];
+      $.isFunction(fn) && fn(motionIntegrator);
+    },
+    bootstrap: function () {
+      this.next();
+    }
+  };
 
   var sidebarToggleLines = {
     lines: [],
@@ -91,7 +104,7 @@ $(document).ready(function () {
 
       $(document)
         .on('sidebar.isShowing', function () {
-          NexT.utils.isDesktop() && $('body').velocity('stop').velocity(
+          isDesktop() && $('body').velocity('stop').velocity(
             {paddingRight: SIDEBAR_WIDTH},
             SIDEBAR_DISPLAY_DURATION
           );
@@ -128,13 +141,7 @@ $(document).ready(function () {
           begin: function () {
             $('.sidebar .motion-element').velocity(
               'transition.slideRightIn',
-              {
-                stagger: 50,
-                drag: true,
-                complete: function () {
-                  self.sidebarEl.trigger('sidebar.motion.complete');
-                }
-              }
+              {stagger: 50, drag: true}
             );
           },
           complete: function () {
@@ -147,7 +154,7 @@ $(document).ready(function () {
       this.sidebarEl.trigger('sidebar.isShowing');
     },
     hideSidebar: function () {
-      NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingRight: 0});
+      isDesktop() && $('body').velocity('stop').velocity({paddingRight: 0});
       this.sidebarEl.find('.motion-element').velocity('stop').css('display', 'none');
       this.sidebarEl.velocity('stop').velocity({width: 0}, {display: 'none'});
 
@@ -167,24 +174,7 @@ $(document).ready(function () {
   };
   sidebarToggleMotion.init();
 
-  NexT.motion.integrator = {
-    queue: [],
-    cursor: -1,
-    add: function (fn) {
-      this.queue.push(fn);
-      return this;
-    },
-    next: function () {
-      this.cursor++;
-      var fn = this.queue[this.cursor];
-      $.isFunction(fn) && fn(NexT.motion.integrator);
-    },
-    bootstrap: function () {
-      this.next();
-    }
-  };
-
-  NexT.motion.middleWares =  {
+  var motionMiddleWares = {
     logo: function (integrator) {
       var sequence = [];
       var $brand = $('.brand');
@@ -199,10 +189,10 @@ $(document).ready(function () {
         o: {duration: 200}
       });
 
-      NexT.utils.isMist() && hasElement([$logoLineTop, $logoLineBottom]) &&
+      isMist() && hasElement([$logoLineTop, $logoLineBottom]) &&
       sequence.push(
-        getMistLineSettings($logoLineTop, '100%'),
-        getMistLineSettings($logoLineBottom, '-100%')
+        getMistLineSettings($logoLineTop, "100%"),
+        getMistLineSettings($logoLineBottom, "-100%")
       );
 
       hasElement($title) && sequence.push({
@@ -281,11 +271,21 @@ $(document).ready(function () {
     },
 
     sidebar: function (integrator) {
-      if (CONFIG.sidebar.display === 'always') {
-        NexT.utils.displaySidebar();
+      if (CONFIG.sidebar === 'always') {
+        displaySidebar();
       }
+      integrator.next();
+    },
+
+    backToTop: function (integrator) {
+      var $b2top = $('.back-to-top');
+      $b2top.on('click', function () {
+        $('body').velocity('scroll');
+      });
       integrator.next();
     }
   };
 
+  window.motionMiddleWares = motionMiddleWares;
+  window.motionIntegrator = motionIntegrator;
 });
